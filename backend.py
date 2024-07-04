2d15473870dc7930c7663c66a65f01f5861306d5
LOLemons <shrimanyu.e@gmail.com>
2024-07-04 19:32:38 +0530

    Outdated code fix
    fixed the issue of the webpage not opening, renamed setup.txt to requirements.txt

--- backend.py
+++ backend.py
@@ -2,8 +2,9 @@
 from flask import Flask, render_template, request
 import fitz
 import torch
-import transformers
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from langchain_huggingface import HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, pipeline, BitsAndBytesConfig
 
 #Set up white paper text for LlaMa prompt
 with fitz.open("sample.pdf", filetype='pdf') as doc: #change the name of the pdf file later
@@ -11,38 +12,52 @@ with fitz.open("sample.pdf", filetype='pdf') as doc: #change the name of the pdf
     for page in doc:
         text += page.get_text()
 
-#model,  tokenizer, and pipeline setup
+#model, tokenizer setup
+quantization_config = BitsAndBytesConfig(load_in_4bit=True)
 model_dir = "C://Users//shrim//Llama-2-7b-hf" #change the directory to the model later
-model = LlamaForCausalLM.from_pretrained(model_dir)
-tokenizer = LlamaTokenizer.from_pretrained(model_dir)
-pipeline = transformers.pipeline(
-"text-generation",
-model=model,
-tokenizer=tokenizer,
-torch_dtype=torch.float16,
-device_map="auto"
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+model = AutoModelForCausalLM.from_pretrained(
+    model_dir, quantization_config=quantization_config, torch_dtype=torch.float16, device_map="auto"
 )
 
-#function to process context and user question with LlaMa
-def model_output(context, question):
-    sequences = pipeline(
-        f"""
+#model generation settings
+generation_config = GenerationConfig.from_pretrained(model_dir)
+generation_config.max_new_tokens = 1024
+generation_config.temperature = 0.0001
+generation_config.top_p = 0.95
+generation_config.do_sample = True
+generation_config.repetition_penalty = 1.15
+
+#pipeline setup
+text_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    generation_config=generation_config,
+)
+
+llm=HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={'temperature':0})
+
+#template and prompt creation
+template ="""<s>[INST] <<SYS>>
 Your purpose is to answer questions about AI, its various implications, factors that must be considered while implementing it, and the responsibilities of corporations and governments when developing and implementing AI.
 Follow exactly these 3 steps:
 1. Read the context below and aggregrate this data
-Context : {context}
+Context : {text}
 2. Answer the question using only this context, use NOTHING ELSE
 User Question: {question}
 
 Remember, you are an honest, helpful, and truthful assistant. Never make up answers on your own. If you don't have any context and are unsure of the answer, or if the question asked by the user is out of context, reply with a witty answer and tell them to ask questions within the context.
-""",
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=400
-    )
-    return sequences
+<</SYS>>[/INST]"""
+
+prompt = PromptTemplate(
+    input_variables=["text", "question"],
+    template=template,
+)
+
+#function to process context and user question with LlaMa
+def model_output(question):
+    return llm(prompt.format(text=text, question=question))
 
 #frontend flask app
 app = Flask(__name__)
@@ -54,10 +69,8 @@ def welcome_page():
 @app.route("/modelResponse", methods=['GET', 'POST'])
 def question_replace():
     user_question = str(request.form["user_question"])
-    output = model_output(text, user_question)
-    for seq in output:
-        model_response = f"{seq['generated_text']}"
-    return render_template("Webpage.html", model_response=model_response)
+    output = model_output(user_question)
+    return render_template("Webpage.html", model_response=output)
 
 if __name__== '__main__':
     app.run(debug=True)
